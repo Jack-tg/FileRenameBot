75becbef19e56abf93f6f18121f50cd0f0d5a303
MY-Tgworld <69492163+MY-Tgworld@users.noreply.github.com>
2020-08-11 07:33:56 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,28 +2,35 @@ class Translation(object):
     START_TEXT = """ <b>I'm A Simple File Renamer Bot With Permanent Thumbnail support!💯</b>
 
 <b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>
-<b>Bot Maintained By: @Mai_bOTs</b>"""
+<b>Bot Maintained By: @noob_admin</b>"""
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    UPGRADE_TEXT = "CONTACT @Noob_admin "
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    UPLOAD_START = "<b>⚡Downloading Completed Now I'm Uploading Into TG⚡</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me JOin > @Mai_bOTs  ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me   ❤️**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @No_OnE_Kn0wS_Me "
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /deletethumbnail❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully😝"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    SAVED_RECVD_DOC_FILE = "<b>♨️File Downloaded Successfully ♨️</b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n Join @Mai_BoTs If You Found Meh Useful</b>"""
+    ABOUT_ME = """ hey mahn😶
+○ My Name : Rename Bot
+○ Creator  : <a href="http://t.me/noob_admin"> ꧁☬⋆ТᎻᎬ༒ᏦᎥᏁᏳ༒C☢nτrͥ☢lͣlͫer⋆☬꧂</a>
+○ Credits : Everyone in this journey
+○ Language : Python3
+○ Source code:<a href="https://t.me.//adhavaabiriyanikittiyalo">👉Click here</a>
+📜 Quote : ആരും പേടിക്കണ്ട എല്ലാവർക്കും കിട്ടും™️"""
+
     HELP_USER = """Ohh You Want Help?? 😅
     
 1.🔹 <b>Send me any Telegram File.</b>

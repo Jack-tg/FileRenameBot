3a08e683fff4b2b084cc7cda776c80e232598626
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-09-09 07:16:15 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 import os
 import time
+import math
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -22,34 +23,26 @@ from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, Filters
 
-from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram
-
+from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
-from database.database import *
 
+active_downloads = []
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["C2F"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
 async def rename_doc(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+        await update.reply_text("You are B A N N E D")
         return
-    TRChatBase(update.from_user.id, update.text, "change")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 70:
+        if len(file_name) > 64:
             await update.reply_text(
                 Translation.IFLONG_FILE_NAME.format(
-                    alimit="70",
+                    alimit="64",
                     num=len(file_name)
                 )
             )
@@ -62,6 +55,13 @@ async def rename_doc(bot, update):
             reply_to_message_id=update.message_id
         )
         c_time = time.time()
+        # cancel stuff
+        global active_downloads
+        if update.chat.id in active_downloads:
+            await a.edit("**Soory 1 Rename Alloved At A Time**")
+            return
+        else:
+            active_downloads.append(update.chat.id)
         the_real_download_location = await bot.download_media(
             message=update.reply_to_message,
             file_name=download_location,
@@ -69,7 +69,8 @@ async def rename_doc(bot, update):
             progress_args=(
                 Translation.DOWNLOAD_START,
                 a,
-                c_time
+                c_time,
+                bot
             )
         )
         if the_real_download_location is not None:
@@ -81,13 +82,6 @@ async def rename_doc(bot, update):
                 )
             except:
                 pass
-            if "IndianMovie" in the_real_download_location:
-                await bot.edit_message_text(
-                    text=Translation.RENAME_403_ERR,
-                    chat_id=update.chat.id,
-                    message_id=a.message_id
-                )
-                return
             new_file_name = download_location + file_name
             os.rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
@@ -98,13 +92,7 @@ async def rename_doc(bot, update):
             logger.info(the_real_download_location)
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
-                mes = await thumb(update.from_user.id)
-                if mes != None:
-                    m = await bot.get_messages(update.chat.id, mes.msg_id)
-                    await m.download(file_name=thumb_image_path)
-                    thumb_image_path = thumb_image_path
-                else:
-                    thumb_image_path = None
+                thumb_image_path = None
             else:
                 width = 0
                 height = 0
@@ -128,14 +116,15 @@ async def rename_doc(bot, update):
                 chat_id=update.chat.id,
                 document=new_file_name,
                 thumb=thumb_image_path,
-                caption=file_name + description,
+                caption=description,
                 # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
                     Translation.UPLOAD_START,
                     a, 
-                    c_time
+                    c_time,
+                    bot
                 )
             )
             try:
@@ -149,9 +138,101 @@ async def rename_doc(bot, update):
                 message_id=a.message_id,
                 disable_web_page_preview=True
             )
+            if (id := update.chat.id) in active_downloads:
+                active_downloads.remove(id)
     else:
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
             reply_to_message_id=update.message_id
         )
+
+
+cancel_button = InlineKeyboardMarkup([[InlineKeyboardButton("cancel", "cancel?download")]])
+
+
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    start,
+    client
+):
+    # cancel stuff
+    global active_downloads
+    if message.chat.id not in active_downloads:
+        await client.stop_transmission()
+    now = time.time()
+    diff = now - start
+    if round(diff % 5.00) == 0 or current == total:
+        # if round(current / total * 100, 0) % 5 == 0:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "[{0}{1}] \nP: {2}%\n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
+
+        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(
+                text="{}\n {}".format(
+                    ud_type,
+                    tmp
+                ),
+                reply_markup=cancel_button
+            )
+        except:
+            pass
+
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+
+@Client.on_callback_query(Filters.create(lambda _, m: m.data == "cancel?download"))
+async def cancel_func(_, callback):
+    global active_downloads
+    try:
+        active_downloads.remove(callback.message.chat.id)
+    except ValueError:
+        await callback.message.edit("Task Already Canceled")
+    else:
+        await callback.message.edit("Task Canceled Successfully")
